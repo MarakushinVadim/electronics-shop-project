11a146861b5ebf15743fde4f4dfeb9ae6fadb533
Marakushin Vadim <marakushin.vadim@gmail.com>
2024-01-27 18:36:15 +0300

    add test

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,11 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+item1 = Item('PC', 2000, 5)
+Item.pay_rate = 0.5
+def test_calculate_total_price():
+    assert Item.calculate_total_price(item1) == 10000
+
+def test_apply_discount():
+    assert Item.apply_discount(item1) == 1000
