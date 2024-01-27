4103dc1d5fb5663c6258a2cf7dc2a53fdaba0d8e
Marakushin Vadim <marakushin.vadim@gmail.com>
2024-01-27 18:39:43 +0300

    add test

--- src/item.py
+++ src/item.py
@@ -5,6 +5,7 @@ class Item:
     pay_rate = 1.0
     all = []
 
+
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
@@ -17,6 +18,7 @@ class Item:
         self.price = price
         self.quantity = quantity
 
+
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
@@ -24,6 +26,7 @@ class Item:
         :return: Общая стоимость товара.
         """
         return self.price * self.quantity
+
 
     def apply_discount(self) -> None:
         """
