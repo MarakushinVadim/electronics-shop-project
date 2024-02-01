3899821e35e3e3b0b0a80182d5f13bba9577db66
Marakushin Vadim <marakushin.vadim@gmail.com>
2024-02-01 23:27:56 +0300

    homework 3

--- src/item.py
+++ src/item.py
@@ -1,9 +1,14 @@
+import csv
+import os.path
+
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
     pay_rate = 1.0
     all = []
+
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
@@ -13,7 +18,10 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.__name = name
+        self.price = int(price)
+        self.quantity = int(quantity)
+
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +29,47 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
+
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price *= self.pay_rate
+        return self.price
+
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        if len(new_name) > 10:
+            self.__name = new_name[:10]
+        else:
+            self.__name = new_name
+
+
+    @classmethod
+    def instantiate_from_csv(cls, file_path):
+        new_path = os.path.abspath(file_path)
+        with open((new_path), 'r', encoding="cp1251") as file:
+            csv_dict = csv.DictReader(file)
+            for item in csv_dict:
+                item1 = Item(item['name'], item['price'], item['quantity'])
+                item1.all.append(item1)
+
+
+    @staticmethod
+    def string_to_number(string):
+        return int(float(string))
+
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
+
+
+    def __str__(self):
+        return f'{self.name}'
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,44 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+item1 = Item('PC', 2000, 5)
+Item.pay_rate = 0.5
+
+
+def test_calculate_total_price():
+    assert item1.calculate_total_price() == 10000
+
+
+def test_apply_discount():
+    assert Item.apply_discount(item1) == 1000
+
+
+def test_name():
+    assert item1.name == 'PC'
+    item1.name = 'Клавиатура'
+    assert item1.name == 'Клавиатура'
+    item1.name = '01234567890'
+    assert item1.name == '0123456789'
+
+
+def test_instantiate_from_csv():
+    item1.instantiate_from_csv('../src/items.csv')
+    assert len(item1.all) == 5
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
+
+
+#item1 = Item("Смартфон", 10000, 20)
+
+
+def test_repr():
+    assert repr(item1) == "Item('PC', 2000, 5)"
+
+
+def test_str():
+    assert str(item1) == 'PC'
