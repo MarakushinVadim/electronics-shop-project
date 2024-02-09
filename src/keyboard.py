f68b07ae441cf0bbfd1f64183938b8a80137c5db
Marakushin Vadim <marakushin.vadim@gmail.com>
2024-02-09 23:43:42 +0300

    homework 4

---
+++ src/keyboard.py
@@ -0,0 +1,28 @@
+from item import Item
+
+
+
+class ChangeLang():
+
+    def __init__(self):
+        self.__language = 'EN'
+
+
+    @property
+    def language(self):
+        return self.__language
+
+
+    def change_lang(self):
+        if self.__language == 'EN':
+            self.__language = 'RU'
+        else:
+            self.__language = 'EN'
+        return self
+
+
+class Keyboard(Item, ChangeLang):
+
+
+    def __init__(self, name, price, quantity):
+        super().__init__(name, price, quantity)
